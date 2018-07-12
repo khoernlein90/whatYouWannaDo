cb58cbde385218be84fd77b5767e1ad3cf94debe
Kyle Hoernlein <khoernlein90@gmail.com>
2018-07-12 16:34:24 -0400

    changed backend keys

--- routes/locator.js
+++ routes/locator.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const axios = require("axios");
+const keys = require("../config/dev");
 
 router.get("/locate", (req, res) => {
-  axios.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAQ1zvpERs8r5Fsmp3gKpsEurMsez2zq3Y").then(data => {
+  axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${keys.googleAPIKey}`).then(data => {
     res.json(data.data);
     console.log(data.data);
   });
